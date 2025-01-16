b7d986a30bd275feeaa6bbf21dcac6775d0d2397
TienVinh <s2gali2s@gmail.com>
2025-01-16 15:03:20 +0700

    tienmvv resposive gpu gridview

--- src/components/GridGPU.tsx
+++ src/components/GridGPU.tsx
@@ -6,15 +6,43 @@ import gpuData from "@/components/common/DataGPU"
 const GridGPU = () => {
     const totalHashRate = gpuData.reduce((total, item) => total + item.staking * item.ib, 0);
     const [isShowMore, setIsShowMore] = useState(false);
-    const [dataPublic, setDataPublic] = useState([...gpuData.slice(0, 3)]);
+    const [windowWidth, setWindowWidth] = useState(
+        typeof window !== "undefined" ? window.innerWidth : 0
+    );
+    const [count, setCount] = useState(3);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (windowWidth >= 768 && windowWidth < 1024) {
+            setCount(4);
+        } else {
+            setCount(3);
+        }
+    }, [windowWidth]);
+
+
+    const [dataPublic, setDataPublic] = useState([...gpuData.slice(0, count)]);
 
     useEffect(() => {
         if (isShowMore) {
             setDataPublic([...gpuData]);
         } else {
-            setDataPublic([...gpuData.slice(0, 3)]);
+            setDataPublic([...gpuData.slice(0, count)]);
         }
-    }, [isShowMore]);
+    }, [isShowMore, count]);
+
+
 
     const handleSetIsShowMore = () => {
         setIsShowMore(!isShowMore);
