7bf7fe1258ebb61dd3a3a62c136cc311335357df
ssm-kiendo <kiendo@saseeme.com>
2025-01-17 16:46:23 +0700

    UpdateScrollAnimation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           setIsInViewport(true); 
         }
       },
-      { threshold: 0.5 }
+      { threshold: 0.2 }
     );
 
     if (infoProductRef.current) {
--- src/components/info/GPURenting.tsx
+++ src/components/info/GPURenting.tsx
@@ -1,116 +1,98 @@
-// components/GPURenting.tsx
 import Image from "next/image";
 import ButtonCustom from "@/components/common/ButtonCustom";
 
-interface GPURentingProps {
+interface GPUMiningProps {
   handleClickRent: () => void;
   handleClickDocTwo: () => void;
 }
 
-const GPURenting = ({ handleClickRent, handleClickDocTwo }: GPURentingProps) => {
+const GPUMining = ({
+  handleClickRent,
+  handleClickDocTwo,
+}: GPUMiningProps) => {
+  const GPUContent = ({
+    isMobile,
+    imageSrc,
+    containerClass,
+    imageClass,
+    buttonContainerClass,
+  }: {
+    isMobile: boolean;
+    imageSrc: string;
+    containerClass: string;
+    imageClass: string;
+    buttonContainerClass: string;
+  }) => (
+    <div className={containerClass}>
+      <Image
+        className={imageClass}
+        src={imageSrc}
+        alt={isMobile ? "commands_mb.png" : "commands.png"}
+        width={isMobile ? 350 : 400}
+        height={356}
+        priority
+      />
+      <div className={buttonContainerClass}>
+        <span className="text-[#F8F8F8F2] font-medium text-[14px] sm:text-[18px] cursor-default text-center">
+        GPU Renting Joining Port
+        </span>
+        <div className="w-full items-center flex flex-col gap-[16px]">
+          <ButtonCustom
+            content="Rent now"
+            onClick={handleClickRent}
+            isActive
+            stylesContent={{
+              fontSize: "14px",
+              fontFamily: "Inter, sans-serif",
+            }}
+            width="130px"
+            height="36px"
+            backgroundColor="#01CAAA"
+            hoverBackgroundColor="#7EFFB2CF"
+            hoverTextColor="black"
+            textColor="black"
+          />
+          <ButtonCustom
+            content="Docs"
+            onClick={handleClickDocTwo}
+            isActive
+            stylesContent={{
+              fontSize: "14px",
+              fontFamily: "Inter, sans-serif",
+            }}
+            width="130px"
+            height="36px"
+            backgroundColor="black"
+            hoverBackgroundColor="#7EFFB2CF"
+            hoverTextColor="black"
+            textColor="#FFFFFF"
+          />
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
-    <div
-      className="w-[100%] lg:w-[497px] h-[356px] hidden sm:flex justify-center items-center 
-           bg-[#76767629] backdrop-blur-[58px] rounded-[8px] 
-           border-b-[2px] sm:border-[0px] border-[#25272670]"
-    >
-      <Image
-        className="object-contain"
-        src="/assets/images/commands.png"
-        alt="education"
-        width={400}
-        height={356}
-        priority
+      <GPUContent
+        isMobile={false}
+        imageSrc="/assets/images/commands.png"
+        containerClass="w-[100%] lg:w-[497px] h-[356px] hidden sm:flex lg:justify-start items-center 
+        bg-[#76767629] backdrop-blur-[58px] rounded-[8px] 
+        border-b-[2px] sm:border-[0px] border-[#25272670] justify-center"
+        imageClass="object-contain"
+        buttonContainerClass="h-[152px] ml-[-80px] mt-[-55px] w-[159px] items-center justify-start sm:justify-between top-[270px] sm:top-[108px] sm:left-[315px] flex flex-col lg:items-start text-center sm:text-left gap-[24px]"
       />
-      <div className="h-[152px] ml-[-80px] mt-[-55px] w-[159px] items-center  justify-start sm:justify-between top-[270px] sm:top-[108px] sm:left-[315px] flex flex-col lg:items-start text-center sm:text-left gap-[24px]">
-        <span className="text-[#F8F8F8F2] font-medium text-[14px] text-center sm:text-[18px] cursor-default">
-          GPU Renting Joining Port
-        </span>
-        <div className="w-full items-center flex flex-col gap-[16px] justify-between">
-          <ButtonCustom
-            content={"Rent now"}
-            onClick={handleClickRent}
-            isActive={true}
-            stylesContent={{
-              fontSize: "14px",
-              fontFamily: "Inter, sans-serif",
-            }}
-            width="130px"
-            height="36px"
-            backgroundColor="#01CAAA"
-            hoverBackgroundColor="#7EFFB2CF"
-            hoverTextColor="black"
-            textColor="black"
-          />
-          <ButtonCustom
-            content={"Docs"}
-            onClick={handleClickDocTwo}
-            isActive={true}
-            stylesContent={{
-              fontSize: "14px",
-              fontFamily: "Inter, sans-serif",
-            }}
-            width="130px"
-            height="36px"
-            backgroundColor="black"
-            hoverBackgroundColor="#7EFFB2CF"
-            hoverTextColor="black"
-            textColor="#FFFFFF"
-          />
-        </div>
-      </div>
-    </div>
-
-    <div className="w-[100%] sm:hidden lg:w-[497px h-[444px] items-center bg-[#76767629] backdrop-blur-[58px] flex justify-center  rounded-[8px] border-b-[2px] sm:border-[0px] border-[#25272670] sm:shadow-none shadow-[inset_2px_4px_16px_2px_rgba(248,248,248,0.06)]">
-      <Image
-        className="object-contain"
-        src="/assets/images/commands_mb.png"
-        alt="education_mb"
-        width={360}
-        height={356}
-        priority
+      <GPUContent
+        isMobile={true}
+        imageSrc="/assets/images/commands_mb.png"
+        containerClass="w-[100%] sm:hidden lg:w-[497px] justify-center h-[444px] items-center bg-[#76767629] backdrop-blur-[58px] flex lg:justify-start  rounded-[8px] border-b-[2px] sm:border-[0px] border-[#25272670] sm:shadow-none shadow-[inset_2px_4px_16px_2px_rgba(248,248,248,0.06)]"
+        imageClass="object-contain"
+        buttonContainerClass="absolute h-[152px] w-[159px] bottom-[7%] flex flex-col text-center gap-[24px]"
       />
-      <div className="absolute h-[152px] w-[159px] bottom-[7%] flex flex-col text-center gap-[24px]">
-        <span className="text-[#F8F8F8F2] font-medium text-center text-[18px] cursor-default">
-          GPU Renting Joining Port
-        </span>
-        <div className="w-full items-center flex flex-col gap-[16px] justify-between">
-          <ButtonCustom
-            content={"Rent now"}
-            onClick={handleClickRent}
-            isActive={true}
-            stylesContent={{
-              fontSize: "14px",
-              fontFamily: "Inter, sans-serif",
-            }}
-            width="130px"
-            height="36px"
-            backgroundColor="#01CAAA"
-            hoverBackgroundColor="#7EFFB2CF"
-            hoverTextColor="black"
-            textColor="black"
-          />
-          <ButtonCustom
-            content={"Docs"}
-            onClick={handleClickDocTwo}
-            isActive={true}
-            stylesContent={{
-              fontSize: "14px",
-              fontFamily: "Inter, sans-serif",
-            }}
-            width="130px"
-            height="36px"
-            backgroundColor="black"
-            hoverBackgroundColor="#7EFFB2CF"
-            hoverTextColor="black"
-            textColor="#FFFFFF"
-          />
-        </div>
-      </div>
-    </div>
-  </>
+    </>
   );
 };
 
-export default GPURenting;
+export default GPUMining;
+
